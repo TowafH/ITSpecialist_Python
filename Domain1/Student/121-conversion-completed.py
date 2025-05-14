c61843e2d809b9d6363a3e6ba18a40fc371a000e
TowafH <towafh@nycstudents.net>
2025-05-14 09:23:11 -0400

    Upload

---
+++ Domain1/Student/121-conversion-completed.py
@@ -0,0 +1,4 @@
+# Rating is supposed to be a whole number
+rating = (input("Enter a rating between 1 and 5: ")) # Default datatype is String
+points = (rating) * 100
+print("You have " + (points) + " to start.")
---
+++ Domain1/Student/123-slicing-completed.py
@@ -0,0 +1,5 @@
+serial_number = "1111-2222-3333-4444"
+print(serial_number[0:4]) # 1111
+
+print(serial_number[-4:]) # 4444
+print(serial_number[5:14]) # 2222-3333

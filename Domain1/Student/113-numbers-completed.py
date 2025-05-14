6885bfe3b0b683fda2ad39508e60661003670c29
TowafH <towafh@nycstudents.net>
2025-05-14 08:46:34 -0400

    Complete 113-numbers

---
+++ Domain1/Student/113-numbers-completed.py
@@ -0,0 +1,12 @@
+number_of_tries = 3
+multiplier = 1.5
+
+# Calculate-1
+print("Additon:", number_of_tries + multiplier)
+print(type(number_of_tries + multiplier))
+
+print("\n")
+
+# Calculate-2
+print("Divison:", number_of_tries / multiplier)
+print(type(number_of_tries / multiplier))

4bd93db7ffd0ab052391bcad1c13582045fca118
TowafH <towafh@nycstudents.net>
2025-05-15 09:05:06 -0400

    Complete

--- Domain1/Student/126-analyze-completed.py
+++ Domain1/Student/126-analyze-completed.py
@@ -1,5 +1,5 @@
 quote = "The quick brown fox jumped over the lazy dog."
-print(quote[4:9])
+print(quote[4:9]) # 'quick'
 
 coins = ('Platinum','Gold','Silver') #What type of data structure is this? - Tuple data structure
 #Can we change Platinum to Bronze? - No, items within a tuple CANNOT be changed
--- Domain1/Student/126-list_operations.py
+++ Domain1/Student/126-list_operations.py
@@ -1,4 +1,5 @@
-capitals=["Montgomery","Juneau","Phoenix"]
-
-
-print(capitals)
+capitals = ["Montgomery","Juneau","Phoenix"]
+capitals.append("Sacramento")
+print(capitals) # Adds Sacramento to the end of the list
+capitals.insert(3, "Little Rock")
+print(capitals) # Adds "Little Rock" before Sacramento

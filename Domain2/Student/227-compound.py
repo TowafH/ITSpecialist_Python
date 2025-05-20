0c44ea363cbafbc494090bfa50dd25121d5df87e
TowafH <towafh@nycstudents.net>
2025-05-20 09:31:44 -0400

    Update

--- Domain2/Student/227-compound.py
+++ Domain2/Student/227-compound.py
@@ -2,7 +2,7 @@ coins = ('Bronze','Silver','Platinum','Gold')
 scepter = False
 for coin in coins:
 
-    if coin == 'Platinum' and scepter == True:
+    if coin == 'Platinum' and scepter == True: # Added an additional conditional to create a compound conditional statement
         print('Congratulations! The platinum coin will move you to the next level!')
         continue
     print ('You possess a', coin, 'coin.')

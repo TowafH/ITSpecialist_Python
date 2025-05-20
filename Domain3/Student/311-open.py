15b33b548562111079a9a790cb7e3cc3c2745443
TowafH <towafh@nycstudents.net>
2025-05-20 09:34:21 -0400

    Upd

--- Domain3/Student/311-open.py
+++ Domain3/Student/311-open.py
@@ -2,5 +2,7 @@ message = open('311-message.txt','w')
 message.write('Testing file for player configuration')
 message.close()
 
-message_test = open('311-message.txt','r')
-print('311-message is open')
+message_test = open('311-message.txt','r') # r --> Read, default value to open a file and outputs an error if the file doesn't exist
+print('311-message is open') 
+# Output:
+# 311-message.txt will be opened 

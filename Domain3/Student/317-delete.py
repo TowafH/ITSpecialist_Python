6870ccfc147c627469cd48d0836ac2c3949731d6
TowafH <towafh@nycstudents.net>
2025-05-22 09:07:41 -0400

    Yes

--- Domain3/Student/317-delete.py
+++ Domain3/Student/317-delete.py
@@ -1,9 +1,11 @@
 import os
 message_file = '317-message.txt'
 
+# ai. Lines 5-7 check if the message_file exists and removes it and prints a message
 if os.path.exists(message_file):
     os.remove(message_file)
     print("Message file removed")
-    
+
+# bi. The else statement on lines 10-11 indiciate if the file doesn't exist
 else: 
     print("There was no message file to remove")

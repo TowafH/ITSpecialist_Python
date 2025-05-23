18b602b3cb6f7ef3237d4be62a6aa46a6bc50518
TowafH <towafh@nycstudents.net>
2025-05-22 23:20:32 -0400

    Complete

--- Domain3/Student/323-items.py
+++ Domain3/Student/323-items.py
@@ -1,1 +1,21 @@
--
+level = int(input("Enter the desired level: "))
+level_1_items = ("Rock", "Pogo Stick")
+level_2_items = ("Wand", "Pogo Stick")
+level_3_items = ("Wand", "Rock", "Pogo Stick")
+
+
+def game(level):
+    if level == 1:
+        for item in level_1_items:
+            print(f"You can get a {item} at level {level}")
+    elif level == 2:
+        for item in level_2_items:
+            print(f"You can get a {item} at level {level}")
+    elif level == 3:
+        for item in level_3_items:
+            print(f"You can get a {item} at level {level}")
+    else:
+        level = int(input("Enter a level between 1 and 3: "))
+        game(level)
+
+game(level)

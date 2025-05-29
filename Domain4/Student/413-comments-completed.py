62c42c8a4dbf96ec477162bc54676ad8976f847a
TowafH <towafh@nycstudents.net>
2025-05-29 08:48:48 -0400

    Submit

---
+++ Domain4/Student/413-comments-completed.py
@@ -0,0 +1,14 @@
+game_state = True
+game_lives = 1
+
+while game_lives <= 3:
+    # Ranges are zero-based
+    for i in range(1,11): 
+        # Loop to print positions achieved during lives
+        print(f"You have reached position {i} in game life {game_lives}")
+    if game_state == True:
+        # game_lives +=1
+        # Adding a hashtag symbol to line 10 prevents the games_lives from being incremented
+print("Thank you for playing.")
+
+# True, each line of code can have more than on inline comment seperated by #'s

dd6b0ab29de80cc631559a94846147cb3a9a578c
TowafH <towafh@nycstudents.net>
2025-05-29 08:43:03 -0400

    Load

--- Domain4/Student/412-white_space.py
+++ Domain4/Student/412-white_space.py
@@ -1,10 +1,14 @@
 game_state= True
 game_lives= 1
-
+# 1ai. We can add a space here to differentiate 
 while game_lives <= 3:
     for i in range(1,11):
-        print(f "You have reached position {i} in game life {game_lives}")
+        print(f"You have reached position {i} in game life {game_lives}")
+        # 1bi. We can fix the unterminated string error by removing the to connect the f with the quotation mark
     if game_state == True:
         game_lives +=1
         
 print("Thank you for playing.")
+
+# 2a. 2 Blank lines should precede function and class definitions
+# 3a. 1 Blank line is used to seperate code blocks within a function

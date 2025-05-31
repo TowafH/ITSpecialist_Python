a763c25c4fd06f5bfc1f9d4256a857dea7fa5fc6
TowafH <towafh@nycstudents.net>
2025-05-31 11:49:18 -0400

    Update

--- Domain6/Student/612-os-completed.py
+++ Domain6/Student/612-os-completed.py
@@ -2,8 +2,12 @@ import os
 
 print("Your current directory is:", os.getcwd()) # Print's the current directory
 
-for text_file in os.listdir():
-    if text_file.endswith('.txt') and not text_file.startswith('OLD'):
+if "612-os.txt" in os.listdir():
+    os.rename("612-os.txt", "OLD612-os.txt")
+    print("Renamed 601-message.txt to OLD601-message.txt")
+
+for text_file in os.listdir():  # os.listdir lists all files and folders in the current working directory.
+    if text_file.endswith('.txt'):
         with open(text_file, 'r') as file:
             content = file.read()
             print(f"--- {text_file} ---")
--- Domain6/Student/613-ospath.py
+++ Domain6/Student/613-ospath.py
@@ -1,15 +1,20 @@
-import os
+import os.path
 
-if not os.path.exists('613-message.txt'):
-    message = open('613-message.txt','w')
+if not os.path.exists('Domain6/Student/613-message.txt'):
+    message = open('Domain6/Student/613-message.txt','w')
     message.write('Testing file for player configuration\n')
     message.write('Testing file for player score')
     print("Configuration file made")
     message.close()
 else: 
-    message_test = open('613-message.txt','r')
+    message_test = open('Domain6/Student/613-message.txt','r')
     content = message_test.read()
     print(content)
     message_test.close()
 
- 
+file_path = os.path.join('Domain6/Student/613-test', 'Domain6/Student/613-test.txt')
+
+if os.path.exists(file_path):
+    print(f"{file_path}: File Exists!")
+else:
+    print(f"{file_path} doesn't exist!")
